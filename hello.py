0f6fa354454a80f9f7291801414427277c01acdb
LucianoBeltrao <lucianoalvesjf@gmail.com>
2023-10-17 01:54:05 +0000

    Ajuste nos arquivos html login/charts/register

--- hello.py
+++ hello.py
@@ -17,11 +17,37 @@ def contatos():
 def index():
     return render_template("index.html")
 
+@app.route('/tables')
+def tables():
+    return render_template("tables.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template("forgot-password.html")
+
+@app.route('/utilitiesanimation')
+def utilitiesanimation():
+    return render_template("utilities-animation.html")
+
+
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+
 
 @app.route('/usuarios/<nome_usuario>')
 def usuarios(nome_usuario):
     return render_template('usuarios.html', nome_usuario=nome_usuario)
-
 
 
 
